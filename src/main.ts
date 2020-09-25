3f7658e54bb9067c3f0d6e42def9f9f2d3b77daa
Tássio <tassio72@hotmail.com>
2020-09-25 19:26:22 -0300

    chore: css variables and welcome message

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,21 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+import { getCssVariableValue } from "@/helpers/styles.ts";
+import "@/assets/styles/variables.css";
+
+const init = () =>
+  createApp(App)
+    .use(store)
+    .use(router)
+    .mount("#app");
+
+const BRANCH_COLOR = getCssVariableValue("--branch-color");
+const BRANCH_COLOR_LIGHTEN = getCssVariableValue("--branch-color-lighten");
+const WELCOME_DEV = "%c Hi, Nutris Dev!! ​😁🍉​";
+const WELCOME_DEV_STYLES = `font-size: 14px; color: ${BRANCH_COLOR_LIGHTEN}; border: 0.5px solid ${BRANCH_COLOR}; border-radius: 2px`;
+
+console.log(
+  process.env.VUE_APP_FIREBASE_PROJECT_ID?.includes("dev") && WELCOME_DEV,
+  WELCOME_DEV_STYLES
+);
