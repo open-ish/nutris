1e968ba8bf742495a4dacb53076ec101b35c7058
Tássio <tassio72@hotmail.com>
2020-09-25 18:06:28 -0300

    feat: add firebase files

---
+++ firebaseApp.ts
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+require('firebase/auth')
+
+console.log(
+  process.env.VUE_APP_FIREBASE_PROJECT_ID?.includes('dev') && 'Hi, Dev!! ​😁🍉​'
+)
+
+const FIREBASE_URL = '.firebaseapp.com'
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + FIREBASE_URL,
+  databaseURL:
+    'https://' + process.env.VUE_APP_FIREBASE_PROJECT_ID + FIREBASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.appspot.com',
+  appId: process.env.VUE_APP_FIREBASE_ID,
+}
+
+const FirebaseApp: any = firebase.initializeApp(config)
+const firestore = firebase.firestore()
+
+FirebaseApp.db = firestore
+FirebaseApp.db.enablePersistence({ synchronizeTabs: true })
+
+export default FirebaseApp
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import FirebaseApp from '@/../firebaseApp'
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+import { User } from '@/models/User.ts'
+
+const init = () =>
+  createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
+
+FirebaseApp.auth().onAuthStateChanged(async (user: User) => {
+  if (user) {
+    // user commit
+  } else {
+    // another
+  }
+  init()
+})
