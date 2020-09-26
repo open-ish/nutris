0aa0e6a25d2bd6723176b183ac656bb38658f6d5
Tássio <tassio72@hotmail.com>
2020-09-26 01:14:48 -0300

    chore: remove img and lint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -127,15 +127,15 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+import { Options, Vue } from 'vue-class-component'
 
 @Options({
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 })
 export default class HelloWorld extends Vue {
-  msg!: string;
+  msg!: string
 }
 </script>
 
--- src/helpers/styles.ts
+++ src/helpers/styles.ts
@@ -1,4 +1,4 @@
 export const getCssVariableValue = (variableName: string) =>
   getComputedStyle(document.documentElement)
     .getPropertyValue(variableName)
-    .trim();
+    .trim()
