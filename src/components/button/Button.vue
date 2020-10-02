9acc4cee06e174b7fa9711628722083dba3e3a15
Tássio <tassio72@hotmail.com>
2020-10-01 23:03:34 -0300

    feat: add btn

---
+++ src/components/button/Button.vue
@@ -0,0 +1,305 @@
+<template>
+  <component
+    :is="href ? 'a' : 'button'"
+    :class="buttonClass"
+    :href="href"
+    :role="href ? '' : 'button'"
+    :aria-busy="isLoading"
+    @click="$emit('click')"
+  >
+    <Spinner
+      v-if="isLoading"
+      :color="spinnerColor"
+      :size="spinnerSize"
+      class="loading"
+    />
+    <span v-if="startIcon" class="startIcon" />
+    <span class="children">
+      <slot />
+    </span>
+    <span v-if="endIcon" class="endIcon" />
+  </component>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+import Spinner from '@/components/spinner/Spinner.vue'
+
+@Options({
+  components: {
+    Spinner,
+  },
+})
+export default class Button extends Vue {
+  @Prop({ default: 'default' }) color!: string
+  @Prop({ default: 'filled' }) variant!: string
+  @Prop({ default: 'round-square' }) shape!: string
+  @Prop({ default: 'medium' }) size!: string
+  @Prop({ default: true }) isLoading!: boolean
+  @Prop({ default: '' }) startIcon!: string
+  @Prop({ default: '' }) endIcon!: string
+  @Prop() href!: string
+
+  get buttonClass() {
+    return [
+      'button',
+      this.color,
+      this.variant,
+      this.shape,
+      this.size,
+      this.isLoading ? 'isLoading' : '',
+    ]
+  }
+
+  get spinnerColor() {
+    return this.variant === 'filled' ? `${this.color}-contrast` : this.color
+  }
+
+  get spinnerSize() {
+    return this.size === 'medium' ? 24 : 36
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  align-items: center;
+  background-color: transparent;
+  border: 1px solid transparent;
+  border-radius: var(--space-lg);
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  letter-spacing: var(--typography-button-letter);
+  outline: 0;
+  overflow: hidden;
+  position: relative;
+  text-decoration: none;
+  transition-duration: var(--transition-duration-speed);
+  transition-property: border-color;
+  will-change: border-color;
+
+  &::before {
+    border-radius: var(--space-lg);
+    bottom: 0;
+    content: '';
+    left: 0;
+    opacity: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transition-duration: var(--transition-duration-speed);
+    transition-property: opacity;
+  }
+
+  &[disabled] {
+    cursor: not-allowed;
+
+    &.text {
+      color: var(--disabled-color);
+    }
+
+    &.outlined {
+      border-color: var(--disabled-color);
+      color: var(--disabled-color);
+    }
+
+    &.filled {
+      background-color: var(--disabled-color);
+      color: var(--disabled-color-contrast);
+    }
+  }
+}
+
+.children {
+  display: inherit;
+  position: relative;
+
+  img {
+    max-width: none;
+  }
+}
+
+.startIcon {
+  display: inline-block;
+  position: relative;
+
+  &:not(:empty) {
+    margin-right: var(--space-xs);
+  }
+}
+
+.endIcon {
+  display: inline-block;
+  position: relative;
+
+  &:not(:empty) {
+    margin-left: var(--space-xs);
+  }
+}
+
+.isLoading {
+  cursor: default;
+
+  .children,
+  .startIcon,
+  .endIcon {
+    opacity: 0;
+  }
+}
+
+.default {
+  &.text {
+    color: var(--default-color);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--default-color-darken);
+        opacity: 0.1;
+      }
+    }
+  }
+
+  &.outlined {
+    border-color: var(--default-color);
+    color: var(--default-color);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--default-color-darken);
+        opacity: 0.1;
+      }
+    }
+  }
+
+  &.filled {
+    background-color: var(--default-color);
+    color: var(--default-color-contrast);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--default-color-darken);
+        opacity: 1;
+      }
+    }
+  }
+}
+
+.primary {
+  &.text {
+    color: var(--primary-color);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--primary-color-darken);
+        opacity: 0.1;
+      }
+    }
+  }
+
+  &.outlined {
+    border-color: var(--primary-color);
+    color: var(--primary-color);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--primary-color-darken);
+        opacity: 0.1;
+      }
+    }
+  }
+
+  &.filled {
+    background-color: var(--primary-color);
+    color: var(--primary-color-contrast);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--primary-color-darken);
+        opacity: 1;
+      }
+    }
+  }
+}
+
+.secondary {
+  &.text {
+    color: var(--secondary-color);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--secondary-color-darken);
+        opacity: 0.1;
+      }
+    }
+  }
+
+  &.outlined {
+    border-color: var(--secondary-color);
+    color: var(--secondary-color);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--secondary-color-darken);
+        opacity: 0.1;
+      }
+    }
+  }
+
+  &.filled {
+    background-color: var(--secondary-color);
+    color: var(--secondary-color-contrast);
+
+    &:not([disabled]):not(.isLoading):hover {
+      &::before {
+        background-color: var(--secondary-color-darken);
+        opacity: 1;
+      }
+    }
+  }
+}
+
+// .small {
+//   height: var(--space-md);
+//   padding-left: var(--space-sm);
+//   padding-right: var(--space-sm);
+
+//   .children {
+//     font-size: 0.8em;
+//   }
+// }
+
+.medium {
+  height: var(--space-md);
+  padding-left: var(--space);
+  padding-right: var(--space);
+
+  .children {
+    font: var(--typography-button-font);
+  }
+}
+
+.large {
+  height: var(--space-lg);
+  padding-left: var(--space);
+  padding-right: var(--space);
+
+  .children {
+    font: var(--typography-title-font);
+  }
+}
+
+.round-square {
+  min-width: var(--space-xlg);
+}
+
+.loading {
+  position: absolute;
+  z-index: var(--zindex-1);
+}
+
+.bold {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/spinner/Spinner.vue
@@ -0,0 +1,94 @@
+<template>
+  <svg
+    :class="['spinner', color]"
+    :style="{ height: `${size}px`, width: `${size}px` }"
+    role="progressbar"
+    aria-busy="true"
+    viewBox="0 0 66 66"
+  >
+    <circle
+      :class="['circle']"
+      fill="none"
+      strokeWidth="6"
+      strokeLinecap="round"
+      cx="33"
+      cy="33"
+      r="30"
+    ></circle>
+  </svg>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+export default class Spinner extends Vue {
+  @Prop({ default: 'default' }) color!: string
+  @Prop({ default: 36 }) size!: number
+}
+</script>
+
+<style lang="scss" scoped>
+$rotation: 270;
+$dashoffset: 180;
+$animation-time: 1s;
+
+.spinner {
+  animation: rotation $animation-time linear infinite;
+}
+
+.circle {
+  animation: turn $animation-time ease-in-out infinite;
+  stroke-dasharray: $dashoffset;
+  stroke-dashoffset: 0;
+  stroke-linecap: round;
+  stroke-width: 6;
+  transform-origin: center;
+}
+
+.default {
+  stroke: var(--default-color);
+}
+
+.default-contrast {
+  stroke: var(--default-color-contrast);
+}
+
+.primary {
+  stroke: var(--primary-color);
+}
+
+.primary-contrast {
+  stroke: var(--primary-color-contrast);
+}
+
+.secondary {
+  stroke: var(--secondary-color);
+}
+
+.secondary-contrast {
+  stroke: var(--secondary-color-contrast);
+}
+
+@keyframes rotation {
+  100% {
+    transform: rotate(#{$rotation}deg);
+  }
+}
+
+@keyframes turn {
+  0% {
+    stroke-dashoffset: $dashoffset;
+  }
+
+  50% {
+    stroke-dashoffset: ($dashoffset / 4);
+    transform: rotate(#{$rotation / 2}deg);
+  }
+
+  100% {
+    stroke-dashoffset: $dashoffset;
+    transform: rotate(#{$rotation * 1.65}deg);
+  }
+}
+</style>
