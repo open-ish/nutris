93f993ad1f171b44f8b5ecc37a15c6c9cb2b6b47
Tássio <tassio72@hotmail.com>
2020-10-03 22:20:26 -0300

    feat: create nav with app xp

---
+++ src/layouts/default/components/nav/Nav.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav class="nav">
+    <router-link class="item g-center--y" to="/">
+      <i class="nutris-ticket"></i>
+      <span>Cálculo avulso</span>
+    </router-link>
+    <router-link class="item g-center--y" to="/">
+      <i class="nutris-users"></i>
+      <span>Meus Pacientes</span>
+    </router-link>
+    <router-link class="item g-center--y" to="/">
+      <i class="nutris-user"></i>
+      <span>Novo paciente</span>
+    </router-link>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
+
+@Options({})
+export default class Nav extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '@/layouts/default/Default-variables.scss';
+$nav-shadow: 0 -1px 3px rgba(0, 0, 0, 0.2);
+
+.nav {
+  background-color: var(--white);
+  bottom: 0;
+  box-shadow: $nav-shadow;
+  display: flex;
+  left: 0;
+  justify-content: space-between;
+  position: fixed;
+  width: 100vw;
+  font: var(--typography-caption-font);
+
+  @media screen and (min-width: $screen-sm) {
+    background-color: transparent;
+    box-shadow: none;
+    justify-content: flex-end;
+    margin-left: auto;
+    position: initial;
+    width: 100%;
+  }
+}
+.item {
+  padding: var(--space-xs);
+  text-align: center;
+
+  [class^='nutris-'] {
+    font-size: 24px;
+  }
+
+  span {
+    white-space: nowrap;
+  }
+
+  @media screen and (min-width: $screen-sm) {
+    color: var(--white);
+    margin-right: var(--space);
+
+    &:hover {
+      color: var(--primary-color-lighten);
+    }
+  }
+}
+</style>
