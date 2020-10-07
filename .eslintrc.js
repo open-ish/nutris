c1da4475cc192093528b02225528b0aeb8e03c07
Joao Matheus <joao.matheus2819@gmail.com>
2020-10-06 21:08:40 -0300

    chore: add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
