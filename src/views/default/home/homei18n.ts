b765038184b545f6761f0daba08efab92dc52c41
Tássio <tassio72@hotmail.com>
2020-10-12 17:50:28 -0300

    chore: lint

--- src/views/default/home/homei18n.ts
+++ src/views/default/home/homei18n.ts
@@ -4,7 +4,7 @@ export enum defaultHomeI18nPath {
   textTwo = 'home.textTwo',
   purpose = 'home.purpose',
   purposeText = 'home.purposeText',
-  purposeButton = 'home.purposeButton'
+  purposeButton = 'home.purposeButton',
 }
 
 export const defaultHomePt = {
@@ -13,9 +13,10 @@ export const defaultHomePt = {
     ' é um Web App para cálculos de dietas parenteral e enteral. Agora ficará facil quantificar a quantidade protéica e calórica de seus pacientes',
   textTwo:
     'E como é um Web App, você não precisa baixa-lo e pode economizar a mémoriade seu celular.',
-    purpose: 'Fins didáticos',
-    purposeText: 'é um projeto open-source criado para apoiar a comunidade de tecnologia. Você pode conhecer mais sobre o',
-    purposeButton: ' projeto no repositório da open-ish.'
+  purpose: 'Fins didáticos',
+  purposeText:
+    'é um projeto open-source criado para apoiar a comunidade de tecnologia. Você pode conhecer mais sobre o',
+  purposeButton: ' projeto no repositório da open-ish.',
 }
 
 export const defaultHomeEn = {
@@ -24,7 +25,8 @@ export const defaultHomeEn = {
     ' is a Web App for calculating parenteral and enteral diets. Now it will be easy to quantify the protein and caloric amount of your patients',
   textTwo:
     "As it is a Web App, you do not need to download it and you can save your phone's memory.",
-    purpose: 'Didactic purposes',
-    purposeText: 'is an open-source project created to support the community of technology. You can learn more about the',
-    purposeButton: ' project at open-ish repository.'
+  purpose: 'Didactic purposes',
+  purposeText:
+    'is an open-source project created to support the community of technology. You can learn more about the',
+  purposeButton: ' project at open-ish repository.',
 }
