122bb98d27ccc43f0450b4114b2f4eea051266be
Tássio <tassio72@hotmail.com>
2020-10-17 17:03:33 -0300

    feat: start manage diets page

--- src/views/default/manage-diets/ManageDiets.vue
+++ src/views/default/manage-diets/ManageDiets.vue
@@ -1,29 +1,45 @@
 <template>
-  <EmptyState @action="action">
-    <h3>Gostaria de cadastrar sua primeira dieta? 😁</h3>
+  <EmptyState v-if="!diets.length" @action="action">
+    <h1>Gostaria de cadastrar sua primeira dieta? 😁</h1>
     <template v-slot:btn>
       Quero Cadastrar!!
     </template>
   </EmptyState>
+  <section v-else data-testid="manage-diets">
+    <FixedBtn
+      mode="insert"
+      startIcon="nutris-plus"
+      aria-label="Sinal de mais"
+    />
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { createNamespacedHelpers } from 'vuex'
+import { mapGetters } from 'vuex'
 
 import EmptyState from '@/components/empty-state/EmptyState.vue'
-import { I18nGetters } from '@/store/i18n/types'
+import FixedBtn from '@/components/button/FixedBtn.vue'
+import { I18nGetters, I18N_NAMESPACE } from '@/store/i18n/types'
+import {
+  ManageDietsGetters,
+  MANAGE_DIETS_NAMESPACE,
+} from '@/store/manage-diets/types'
 
-const { mapGetters } = createNamespacedHelpers('i18n')
+// const { mapGetters } = createNamespacedHelpers('i18n')
 
 export default defineComponent({
   name: 'ManageDiets',
   components: {
     EmptyState,
+    FixedBtn,
   },
   computed: {
-    ...mapGetters({
+    ...mapGetters(I18N_NAMESPACE, {
       language: I18nGetters.LANGUAGE,
+    }),
+    ...mapGetters(MANAGE_DIETS_NAMESPACE, {
+      diets: ManageDietsGetters.DIETS,
     }),
   },
   methods: {
