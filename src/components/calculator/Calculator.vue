acb3568dc3d79eaba78ba8b28344bc8d3fc485e6
Tássio <tassio72@hotmail.com>
2020-12-07 17:09:23 -0300

    feat: insert parenteral func

--- src/components/calculator/Calculator.vue
+++ src/components/calculator/Calculator.vue
@@ -1,59 +1,66 @@
 <template>
-  <!-- @click="toggleRemoveModal" v-if="isOpenModal" -->
-  <Modal>
-    <div class="calculator">
-      <div class="info">
-        <h3>Calculadora</h3>
-        <p>Paciente: NOME</p>
-      </div>
-      <Select
-        class="calculator-input"
-        label="Dieta base"
-        :options="[{ text: 'um' }, { text: 'dois' }]"
-        v-model:value="select"
-      />
-      <Input
-        class="calculator-input"
-        v-model:value="input"
-        type="number"
-        label="Massa corporal/peso atual (em kg)"
-      />
-      <Input
-        class="calculator-input"
-        v-model:value="input"
-        type="number"
-        label="Meta calórica (em kcal/kg)"
-      />
-      <Input
-        class="calculator-input"
-        v-model:value="input"
-        type="number"
-        label="Meta protéica (em gPTN/kg)"
-      />
-      <Input
-        class="calculator-input"
-        v-model:value="input"
-        type="number"
-        label="Volume de dieta recebido (em mL)"
-      />
-      <Button color="primary"> Calcular </Button>
+  <div class="calculator">
+    <div class="info">
+      <h3>Calculadora</h3>
+      <p v-if="patient">Paciente: {{ patient.anonymousIdentifier }}</p>
     </div>
-  </Modal>
+    <Select
+      class="calculator-input"
+      label="Dieta base"
+      :objectInstead="true"
+      :options="
+        diets.map((diet) => {
+          return { ...diet, text: diet.name }
+        })
+      "
+      v-model:value="diet"
+    />
+    <Input
+      class="calculator-input"
+      v-model:value="body"
+      type="number"
+      label="Massa corporal/peso atual (em kg)"
+    />
+    <Input
+      class="calculator-input"
+      v-model:value="calGoal"
+      type="number"
+      label="Meta calórica (em kcal/kg)"
+    />
+    <Input
+      class="calculator-input"
+      v-model:value="proteinGoal"
+      type="number"
+      label="Meta protéica (em gPTN/kg)"
+    />
+    <Input
+      class="calculator-input"
+      v-model:value="volumeReceived"
+      type="number"
+      label="Volume de dieta recebido (em mL)"
+    />
+    <Button @click="calculate()" color="primary"> Calcular </Button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue'
+import { defineComponent, PropType, ref } from 'vue'
 import { createNamespacedHelpers } from 'vuex'
+import { parenteral } from '@open-ish/nutris-roles'
 
 // import { getAge } from '@/helpers/date/date'
 import { Patient } from '@/models/Patient'
+import {
+  ManageDietsGetters,
+  MANAGE_DIETS_NAMESPACE,
+} from '@/store/manage-diets/types'
 import { PatientsGetters, PATIENTS_NAMESPACE } from '@/store/patients/types'
-
-import Modal from '@/components/modal/Modal.vue'
 import Input from '@/components/form/input/Input.vue'
 import Select from '@/components/form/select/Select.vue'
 import Button from '@/components/form/button/Button.vue'
+import { Diet } from '@/models/Diet'
 
+const DIETS_MAPS = createNamespacedHelpers(MANAGE_DIETS_NAMESPACE)
 const PATIENTS_MAPS = createNamespacedHelpers(PATIENTS_NAMESPACE)
 
 export default defineComponent({
@@ -62,26 +69,50 @@ export default defineComponent({
     id: {
       type: String,
     },
+    patient: {
+      type: Object as PropType<Patient>,
+    },
   },
   components: {
     Input,
     Select,
     Button,
-    Modal,
   },
   setup() {
-    const patient = reactive({} as Patient)
-    const select = ref('')
-    const input = ref('')
-    return { patient, select, input }
+    const body = ref('')
+    const diet = ref({} as Diet)
+    const calGoal = ref('')
+    const proteinGoal = ref('')
+    const volumeReceived = ref('')
+    return { body, calGoal, proteinGoal, volumeReceived, diet }
   },
   computed: {
+    ...DIETS_MAPS.mapGetters({
+      diets: ManageDietsGetters.DIETS,
+    }),
     ...PATIENTS_MAPS.mapGetters({
       findPatient: PatientsGetters.FIND_PATIENT,
     }),
   },
   created() {
     // this.patient = this.findPatient(this.id)
+  },
+  methods: {
+    calculate() {
+      const data = {
+        patient: {
+          body: Number(this.body),
+          calGoal: Number(this.calGoal),
+          proteinGoal: Number(this.proteinGoal),
+          volumeReceived: Number(this.volumeReceived),
+        },
+        diet: {
+          cal: Number(this.diet.calAmount),
+          protein: Number(this.diet.proteinAmount),
+        },
+      }
+      console.log('paraenteral', parenteral(data))
+    },
   },
 })
 </script>
@@ -99,9 +130,6 @@ export default defineComponent({
 .info {
   align-self: flex-start;
   margin-bottom: var(--space-sm);
-  // display: flex;
-  // flex-direction: column;
-  // align-items: flex-start;
 }
 
 .button {
