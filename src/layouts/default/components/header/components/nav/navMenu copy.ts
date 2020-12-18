e4b3f203e836a430521a2c23dffc13221d91344d
Tássio <tassio72@hotmail.com>
2020-12-17 22:00:29 -0300

    feat: change nav

---
+++ src/layouts/default/components/header/components/nav/navMenu copy.ts
@@ -0,0 +1,28 @@
+import { navI18nPath } from '@/layouts/default/components/header/components/nav/nav.i18n.ts'
+import { Paths } from '@/router/default/enums'
+
+export const navOptions = [
+  // I will think if it is necessary
+  // {
+  //   path: Paths.calculate,
+  //   i18n: navI18nPath.calculate,
+  //   icon: 'nutris-ticket',
+  // },
+  {
+    path: Paths.patients,
+    i18n: navI18nPath.patients,
+    icon: 'nutris-users',
+  },
+  {
+    path: Paths.patients + '/new',
+    i18n: navI18nPath.newPacient,
+    icon: 'nutris-user',
+  },
+  {
+    path: Paths.manageDiets,
+    i18n: navI18nPath.manageDiets,
+    icon: 'nutris-checklist',
+    callBack: 'diets',
+    loading: false,
+  },
+]
