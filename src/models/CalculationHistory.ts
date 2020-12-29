154a934316598c6e0b0857825f14541416de2e80
Tássio <tassio72@hotmail.com>
2020-12-28 22:54:14 -0300

    chore: create new interface

--- src/models/CalculationHistory.ts
+++ src/models/CalculationHistory.ts
@@ -10,3 +10,21 @@ export interface CalculationHistory {
   diet: Diet
   createdAt: number | Date
 }
+
+export interface CalculatorIntermediate {
+  parenteralResult: null
+  patient: {
+    body?: number
+    currentBody: number
+    calGoal: number
+    proteinGoal: Number
+    volumeReceived: Number
+  }
+  diet: {
+    cal?: Number
+    calAmount: Number
+    protein?: Number
+    proteinAmount: Number
+  }
+  createdAt?: number
+}
